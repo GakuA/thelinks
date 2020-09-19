2668d9cc944148c1985afdab5c40d4b5546ae17f
GakuA <akazawagaku@gmail.com>
2020-09-19 11:12:22 +0900

    img

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 	</head>
 	<body>
 		hello
-		<?php echo(file_put_contents("capture/test.text", "world")) ?>
+		<?php
+			//画像のパス
+			$image_path = "https://s.wordpress.com/mshots/v1/https://www.youtube.com/watch?v=YLBM-H2r6-c?w=300&h=200";
+			//保存するファイル名
+			$file_name = 'test.jpg';
+
+			$image = file_get_contents($image_path);
+			$save_path = "capture/".$file_name;
+			while (!file_put_contents($save_path, $image)) {
+				echo "try";
+			};
+		?>
 	</body>
 </html>
