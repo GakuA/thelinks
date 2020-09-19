ceeb64431e6bac4591a91dc449c940bfc976bf0b
GakuA <akazawagaku@gmail.com>
2020-09-19 11:23:55 +0900

    img

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 			//画像のパス
 			$image_path = "https://s.wordpress.com/mshots/v1/https://www.youtube.com?w=300&h=200";
 			//保存するファイル名
-			$file_name = 'test';
+			$file_name = 'test.jpg';
 			while (!$image = file_get_contents($image_path)) {
 				echo "tryDL";
 			}
-
+			echo ($image);
 			$save_path = "capture/".$file_name;
 			while (!file_put_contents($save_path, $image)) {
 				echo "tryUL";
