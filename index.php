77e343dc7c8143cc27dc6150a34443459fe43f92
GakuA <akazawagaku@gmail.com>
2020-09-20 02:47:32 +0900

    url

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 
 		$request = $client->getMessageFactory()->createCaptureRequest();
 		$request->setTimeout(5000);
-		$request->setUrl('https://m.youtube.com/watch?v=wOsYY4UzdkM');
+		$request->setUrl('https://news.yahoo.co.jp/pickup/6371549');
 		var_dump($request);
 		// サイズ指定
 		$width = 800;
