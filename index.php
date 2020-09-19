5f96a39d89fdb18bcf2af45e68150eea4c172685
GakuA <akazawagaku@gmail.com>
2020-09-20 01:52:05 +0900

    10

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
 		$client = Client::getInstance();
 		while (!$request  = $client->getMessageFactory()->createCaptureRequest('https://m.youtube.com/watch?v=0FTTildpyt4')) {}
-		$request->setTimeout(5000);
+		$request->setTimeout(10000);
 		// サイズ指定
 		$width = 800;
 		$height = 600;
