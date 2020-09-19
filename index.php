751c2f1893adced13f2b6463a42e6d67575e89c0
GakuA <akazawagaku@gmail.com>
2020-09-19 21:04:22 +0900

    test

--- index.php
+++ index.php
@@ -10,12 +10,11 @@
 		<img src="https://s.wordpress.com/mshots/v1/https://m.youtube.com/watch?v=0FTTildpyt4?w=400&h=300">
 		-->
 		<?php
-		$url = "https://s.wordpress.com/mshots/v1/https://m.youtube.com/watch?v=0FTTildpyt4?w=400&h=300.jpg";
+		$url = "https://pbs.twimg.com/profile_banners/42566884/1526165516/1500x500";
 /*
 		while (!$img = file_get_contents($url)) {}
+		*/
 		while (!$img = file_get_contents($url)) {}
-*/
-		$img = file_get_contents($url);
 		$img_name = "capture/file.jpg";
 
 		//画像を保存
