024f3d5213c8e841b554180cdafb36818f84778f
GakuA <akazawagaku@gmail.com>
2020-09-20 01:26:41 +0900

    path

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 		use JonnyW\PhantomJs\Client;
 
 		$client = Client::getInstance();
-
+		$client->getEngine()->setPath('/bin/phantomjs'); // phantomjs path
 		while (!$request  = $client->getMessageFactory()->createCaptureRequest('https://m.youtube.com/watch?v=0FTTildpyt4', 'GET', 10000)) {}
 		//set_time_limit(2);
 		$response = $client->getMessageFactory()->createResponse();
