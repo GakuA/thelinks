21ad16b174d761fc6f07a4559a43dca2bbc4316f
GakuA <akazawagaku@gmail.com>
2020-09-19 15:56:57 +0900

    test

--- index.php
+++ index.php
@@ -8,25 +8,17 @@
 		hello
 		<?php
 		require_once( 'vendor/autoload.php' );
-		echo("world");
 		use JonnyW\PhantomJs\Client;
 
-		echo("world2");
 		$client = Client::getInstance();
 
-		echo("world3");
-		$request  = $client->getMessageFactory()->createCaptureRequest('https://www.youtube.com/watch?v=CeVOKBEYebs');
-		echo("world4");
+		$request  = $client->getMessageFactory()->createCaptureRequest('https://www.tokyo-felicia.com/');
 		$response = $client->getMessageFactory()->createResponse();
-		echo("world5");
 
 		// ファイルの保存先を指定する
 		$file = 'capture/file.jpg';
-var_dump($request);
 		$request->setOutputFile($file);
-		echo("world6");
 		$client->send($request, $response);
-		echo("world7");
 		?>
 	</body>
 </html>
