c47ff395a368c8dcf7b6b55b37d01e378a551be6
GakuA <akazawagaku@gmail.com>
2020-09-26 14:45:50 +0900

    logo

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 		<title>LINKS</title>
 	</head>
 	<body>
+		<div>
+			<img src="img/logo.png">
+		</div>
 		hello
 		<img src="https://s.wordpress.com/mshots/v1/https://www.youtube.com/watch?v=pJkMTYvZu0w?w=400&h=300">
 		<?php
