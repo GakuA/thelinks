3f163d30cc925de2aaf9c7762885bf0841366482
GakuA <akazawagaku@gmail.com>
2020-09-26 16:10:05 +0900

    ...

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 			<div class="link">
 				<a target="_blank" href="怖い怖い怖い！！ビクロイ獲るまでずっと喋ってた・・・【 野性爆弾 ロッシー 】 - YouTube">
 					<img src="https://s.wordpress.com/mshots/v1/https://www.youtube.com/watch?v=pJkMTYvZu0w?w=400&h=300">
+					<span>怖い怖い怖い！！ビクロイ獲るまでずっと喋ってた・・・【 野性爆弾 ロッシー 】 - YouTube</span>
 				</a>
-				<span>怖い怖い怖い！！ビクロイ獲るまでずっと喋ってた・・・【 野性爆弾 ロッシー 】 - YouTube</span>
 			</div>
 		</div>
 		<?php
