166371ebc13e8f2c1ac482f6676b5a183d6b25ba
GakuA <akazawagaku@gmail.com>
2020-09-28 00:11:55 +0900

    yomikomi

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="js/load.js"></script>
 		<script src="js/function.js"></script>
 
 		<title>LINKS</title>
