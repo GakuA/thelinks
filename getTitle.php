7ef8a34c8c471926161aa1ed755bb83ee1ff93c3
GakuA <akazawagaku@gmail.com>
2020-09-27 14:10:22 +0900

    3try

--- getTitle.php
+++ getTitle.php
@@ -1,8 +1,16 @@
 <?php
 	$html = @file_get_contents($_POST["postUrl"]);
 
-	if(preg_match("/<title>(.*?)<\/title>/i", $html, $matches)){
-		echo $matches[1];
-	} else {
-		return false;
+	$count = 1;
+	while (!$title=title() || $count <= 3) {
+		$count++;
+	}
+	echo $title;
+
+	function title() {
+		if(preg_match("/<title>(.*?)<\/title>/i", $html, $matches)){
+			return $matches[1];
+		} else {
+			return false;
+		}
 	}
