cb1e4513638d1199b708cda930d841ef27110a98
GakuA <akazawagaku@gmail.com>
2020-09-27 12:58:15 +0900

    php

--- getTitle.php
+++ getTitle.php
@@ -2,7 +2,7 @@
 	$html = @file_get_contents($_POST["postUrl"]);
 
 	if(preg_match("/<title>(.*?)<\/title>/i", $html, $matches)){
-		return $matches[1];
+		echo $matches[1];
 	} else {
 		return false;
 	}
