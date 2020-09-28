f3c321248d18c6d584334e47a90683045c611587
GakuA <akazawagaku@gmail.com>
2020-09-28 12:57:09 +0900

    cron

---
+++ cron.php
@@ -0,0 +1,15 @@
+<?php
+	$link = pg_connect('host=ec2-54-146-4-66.compute-1.amazonaws.com dbname=d92g50uce9vjgl user=zkwliroqdgiofi password=e63c65c7f1f58c3c4a2f30b27e52d34a736e0e0cf6f76f9abee7f663948bc692');
+	if (!$link) {
+		die('接続失敗です。');
+	}
+/*
+	$sql = "CREATE TABLE link(url text, title text, date numeric)";
+	$result_flag = pg_query($sql);
+
+	if (!$result_flag) {
+		exit('INSERTクエリーが失敗しました。');
+	}
+*/
+	echo time();
+	pg_close($link);
