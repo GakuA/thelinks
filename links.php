fa27a74ae634c6e6e9253698192c867b27805f84
GakuA <akazawagaku@gmail.com>
2020-09-30 01:38:49 +0900

    7ad

--- links.php
+++ links.php
@@ -10,15 +10,16 @@
 		exit('SELECTクエリーが失敗しました。');
 	}
 
-	$linkNo = 0;
+	$linkNo = 1;
 	while($row = pg_fetch_assoc($result)){
-		if ($linkNo % 6 == 0 && $linkNo > 0) {
-			echo '<div class="link ad"><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({});</script></div>';
-		}
 		$url = $row["url"];
 		$title = $row["title"];
 		$html = '<div class="link"><a target="_blank" href="'.$url.'"><img src="https://s.wordpress.com/mshots/v1/'.$url.'?w=200&h=150"><span>'.$title.'</span></a></div>';
 		echo $html;
+
+		if ($linkNo % 6 == 0) {
+			echo '<div class="link ad"><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({});</script></div>';
+		}
 
 		$linkNo++;
 	}
