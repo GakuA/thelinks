94410cd49cb641f84a3a3e153843291cc2324264
GakuA <akazawagaku@gmail.com>
2020-10-23 09:05:44 +0900

    rakuten

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="js/load.js"></script>
 		<script src="js/function.js"></script>
 
@@ -37,69 +36,8 @@
 			</div>
 			<input id="postBtn" type="button" value="リンクを貼る">
 		</div>
-<!--
-		<div id="links">
-		</div>
--->
 
 		<div id="links">
-			<div class="link"><a target="_blank" href="https://camp-fire.jp/projects/view/335834?list=music_popular"><img src="https://s.wordpress.com/mshots/v1/https://camp-fire.jp/projects/view/335834?list=music_popular?w=200&amp;h=150"><span>2020年を一生に一度の思い出に。森恵 無観客ライブin沖縄 応援プロジェクト - CAMPFIRE (キャンプファイヤー)</span></a></div>
-			<div class="link"><a target="_blank" href="https://president.jp/articles/-/29483?page=1"><img src="https://s.wordpress.com/mshots/v1/https://president.jp/articles/-/29483?page=1?w=200&amp;h=150"><span>大学デビューで｢レーシック失敗｣した人の末路 電灯がまぶしくて目を開けられない | PRESIDENT Online（プレジデントオンライン）</span></a></div>
-			<div class="link ad">
-				<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961" data-adsbygoogle-status="done"></ins>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
-			</div>
-			<div class="link"><a target="_blank" href="https://www.news24.jp/articles/2020/10/18/07743519.html"><img src="https://s.wordpress.com/mshots/v1/https://www.news24.jp/articles/2020/10/18/07743519.html?w=200&amp;h=150"><span>調布市の住宅街で道路が陥没　立ち入り制限｜日テレNEWS24</span></a></div>
-			<div class="link"><a target="_blank" href="https://news.livedoor.com/article/detail/19073408/"><img src="https://s.wordpress.com/mshots/v1/https://news.livedoor.com/article/detail/19073408/?w=200&amp;h=150"><span>ベルギー、1日の感染者数が1万人超 新型コロナ第2波が深刻化 - ライブドアニュース</span></a></div>
-			<div class="link"><a target="_blank" href="https://news.mynavi.jp/article/20201017-1419705/"><img src="https://s.wordpress.com/mshots/v1/https://news.mynavi.jp/article/20201017-1419705/?w=200&amp;h=150"><span>松本人志、フジで新番組始動　寄席・ドキュメンタル・大喜利で「攻めた企画」 | マイナビニュース</span></a></div>
-			<div class="link"><a target="_blank" href="http://blog.livedoor.jp/dqnplus/archives/2003785.html"><img src="https://s.wordpress.com/mshots/v1/http://blog.livedoor.jp/dqnplus/archives/2003785.html?w=200&amp;h=150"><span>痛いニュース(ﾉ∀`) : 【動画】「降りてこいや」あおり運転し威嚇…被害者「車2台に挟み撃ちされ5人に囲まれ...死を覚悟した」 - ライブドアブログ</span></a></div>
-			<div class="link"><a target="_blank" href="https://www.youtube.com/watch?v=tY8rO5WMRXA"><img src="https://s.wordpress.com/mshots/v1/https://www.youtube.com/watch?v=tY8rO5WMRXA?w=200&amp;h=150"><span>アシカショーハプニング１ - YouTube</span></a></div>
-			<div class="link"><a target="_blank" href="https://goo.gl/maps/HZYME3q79JQsHt728"><img src="https://s.wordpress.com/mshots/v1/https://goo.gl/maps/HZYME3q79JQsHt728?w=200&amp;h=150"><span>	Google Maps	</span></a></div>
-			<div class="link ad">
-				<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961" data-adsbygoogle-status="done"></ins>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
-			</div>
-			<div class="link"><a target="_blank" href="https://datsusalaryman.hatenablog.com/entry/20200801/1596239664"><img src="https://s.wordpress.com/mshots/v1/https://datsusalaryman.hatenablog.com/entry/20200801/1596239664?w=200&amp;h=150"><span>Amazon | 置き配しないをデフォルトにするには - 「非」備忘６。</span></a></div>
-			<div class="link"><a target="_blank" href="https://www.nikkei.com/article/DGXMZO64946020T11C20A0MM8000/"><img src="https://s.wordpress.com/mshots/v1/https://www.nikkei.com/article/DGXMZO64946020T11C20A0MM8000/?w=200&amp;h=150"><span>ソフトバンク、月5000円以下で20～30GB　ドコモの3割安　　:日本経済新聞</span></a></div>
-			<div class="link"><a target="_blank" href="https://minhyo.jp/"><img src="https://s.wordpress.com/mshots/v1/https://minhyo.jp/?w=200&amp;h=150"><span>みんなの評判ランキング｜ 生の声を届ける口コミ投稿サイト</span></a></div>
-			<div class="link"><a target="_blank" href="https://yuichinanjo.com/"><img src="https://s.wordpress.com/mshots/v1/https://yuichinanjo.com/?w=200&amp;h=150"><span>YUICHI NANJO</span></a></div>
-			<div class="link"><a target="_blank" href="https://lifemagazine.yahoo.co.jp/articles/40835"><img src="https://s.wordpress.com/mshots/v1/https://lifemagazine.yahoo.co.jp/articles/40835?w=200&amp;h=150"><span>Yahoo!ライフマガジン - もっと便利に、もっと楽しく毎日を過ごそう</span></a></div>
-			<div class="link"><a target="_blank" href="https://www.microsoft.com/ja-jp/biz/eos#:~:text=2020%20%E5%B9%B4%2010%20%E6%9C%88%2013,%E3%82%92%E3%81%8A%E5%8B%A7%E3%82%81%E3%81%84%E3%81%9F%E3%81%97%E3%81%BE%E3%81%99%E3%80%82"><img src="https://s.wordpress.com/mshots/v1/https://www.microsoft.com/ja-jp/biz/eos#:~:text=2020%20%E5%B9%B4%2010%20%E6%9C%88%2013,%E3%82%92%E3%81%8A%E5%8B%A7%E3%82%81%E3%81%84%E3%81%9F%E3%81%97%E3%81%BE%E3%81%99%E3%80%82?w=200&amp;h=150"><span>2020 年 世代交代。 Windows 7 Office 2010 サポート終了。今から始める、移行への道 - Windows for business</span></a></div>
-			<div class="link ad">
-				<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961" data-adsbygoogle-status="done"></ins>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
-			</div>
-			<div class="link"><a target="_blank" href="https://youtu.be/EcyS2XaEO_c"><img src="https://s.wordpress.com/mshots/v1/https://youtu.be/EcyS2XaEO_c?w=200&amp;h=150"><span>朝倉未来と合同練習＆パフェデート - YouTube</span></a></div>
-			<div class="link"><a target="_blank" href="https://gxyt4.app.goo.gl/jN6Ym"><img src="https://s.wordpress.com/mshots/v1/https://gxyt4.app.goo.gl/jN6Ym?w=200&amp;h=150"><span>ONE Championship 2020 - 10.9 シンガポール大会 - 第5試合 アレクシ・トイヴォネン vs リース・マクラーレン | 無料で動画＆見逃し配信を見るなら【ABEMAビデオ】</span></a></div>
-			<div class="link"><a target="_blank" href="https://gekiura.com/post/34809"><img src="https://s.wordpress.com/mshots/v1/https://gekiura.com/post/34809?w=200&amp;h=150"><span>トリキの錬金術を超える最強のGo To Eat錬金を発見｜激裏情報</span></a></div>
-			<div class="link"><a target="_blank" href="https://departuredoor.herokuapp.com/"><img src="https://s.wordpress.com/mshots/v1/https://departuredoor.herokuapp.com/?w=200&amp;h=150"><span>旅立ちの扉</span></a></div>
-			<div class="link"><a target="_blank" href="https://www.mangaz.com/"><img src="https://s.wordpress.com/mshots/v1/https://www.mangaz.com/?w=200&amp;h=150"><span>無料で漫画が全巻読み放題！ - マンガ図書館Z</span></a></div>
-			<div class="link"><a target="_blank" href="http://kowabana.jp/"><img src="https://s.wordpress.com/mshots/v1/http://kowabana.jp/?w=200&amp;h=150"><span>怖い話投稿サイト 怖話（こわばな）</span></a></div>
-			<div class="link ad">
-				<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961" data-adsbygoogle-status="done"></ins>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
-			</div>
-			<div class="link"><a target="_blank" href="https://news.livedoor.com/article/detail/19029575/"><img src="https://s.wordpress.com/mshots/v1/https://news.livedoor.com/article/detail/19029575/?w=200&amp;h=150"><span>算数の授業で「おつりって何？」子どもの現金離れに共感する声も - ライブドアニュース</span></a></div>
-			<div class="link"><a target="_blank" href="https://news.mynavi.jp/article/20201009-1385222/"><img src="https://s.wordpress.com/mshots/v1/https://news.mynavi.jp/article/20201009-1385222/?w=200&amp;h=150"><span>ExcelやWordなど多数のマイクロソフト製品が10月13日にサポート終了 | マイナビニュース</span></a></div>
-			<div class="link"><a target="_blank" href="https://gekiura.com/"><img src="https://s.wordpress.com/mshots/v1/https://gekiura.com/?w=200&amp;h=150"><span>激裏情報</span></a></div>
-			<div class="link"><a target="_blank" href="https://www.gizmodo.jp/2020/10/coleman-infinity-chair.html?utm_source=facebook&amp;utm_medium=feed&amp;utm_campaign=6d2f887f203905abb0bb1217776149aa&amp;fbclid=IwAR0cztvJ9v1InhyHUwfhHy7TVvm2eNQp012xJIdPKpYtESj2O98fnz_uyVo"><img src="https://s.wordpress.com/mshots/v1/https://www.gizmodo.jp/2020/10/coleman-infinity-chair.html?utm_source=facebook&amp;utm_medium=feed&amp;utm_campaign=6d2f887f203905abb0bb1217776149aa&amp;fbclid=IwAR0cztvJ9v1InhyHUwfhHy7TVvm2eNQp012xJIdPKpYtESj2O98fnz_uyVo?w=200&amp;h=150"><span>一度使ったら手放せない。コールマンの｢インフィニティチェア｣があると、ダメになっちゃう… | ギズモード・ジャパン</span></a></div>
-			<div class="link"><a target="_blank" href="http://blog.livedoor.jp/dqnplus/archives/2003629.html"><img src="https://s.wordpress.com/mshots/v1/http://blog.livedoor.jp/dqnplus/archives/2003629.html?w=200&amp;h=150"><span>痛いニュース(ﾉ∀`) : トレンディエンジェル・斎藤、背骨圧迫骨折の重傷　フジテレビ「でんじろうのTHE実験」ロケで - ライブドアブログ</span></a></div>
-			<div class="link"><a target="_blank" href="https://www.youtube.com/watch?v=MGcJ8w8AAX0"><img src="https://s.wordpress.com/mshots/v1/https://www.youtube.com/watch?v=MGcJ8w8AAX0?w=200&amp;h=150"><span>EP 1 冒険のはじまり | フィッシャーズと失われし碧き秘宝 - YouTube</span></a></div>
-			<div class="link ad">
-				<script async="" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961" data-adsbygoogle-status="done"></ins>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
-			</div>
-			<div class="link"><a target="_blank" href="https://autabi.com/event/event-open/14458/"><img src="https://s.wordpress.com/mshots/v1/https://autabi.com/event/event-open/14458/?w=200&amp;h=150"><span>【10月16日】フジテレビ国際開発局プロデュース協力、イタリア・ローマの人々に会いに行く！ | あうたびオンラインツアー</span></a></div>
 		</div>
 
 		<div id="modal">
--- links.php
+++ links.php
@@ -18,7 +18,7 @@
 		echo $html;
 
 		if ($linkNo % 6 == 0) {
-			echo '<div class="link ad"><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-7x+eo+1+2-5" data-ad-client="ca-pub-3505153643581987" data-ad-slot="6072094961"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({});</script></div>';
+			echo '<div class="link ad"><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="142a8617.af4bd4b0.142a8618.1730d9f1";rakuten_items="ranking";rakuten_genreId="0";rakuten_size="200x200";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";rakuten_ts="1603296752086";</script><script type="text/javascript" src="https://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script></div>';
 		}
 
 		$linkNo++;
