eca58f369d09ca3e9f77fba652f4e674a4ab3f5c
GakuA <akazawagaku@gmail.com>
2020-10-23 09:24:34 +0900

    rakuten

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 			</div>
 			<input id="postBtn" type="button" value="リンクを貼る">
 		</div>
-<script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="142a8617.af4bd4b0.142a8618.1730d9f1";rakuten_items="ctsmatch";rakuten_genreId="0";rakuten_size="200x200";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";rakuten_ts="1603412175006";</script><script type="text/javascript" src="https://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script>
+
 		<div id="links">
 		</div>
 
