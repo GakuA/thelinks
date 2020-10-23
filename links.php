05d47e68812d296c4c7c5d76be0bc8d5fb1ad46f
GakuA <akazawagaku@gmail.com>
2020-10-23 09:47:33 +0900

    test

--- links.php
+++ links.php
@@ -10,7 +10,7 @@
 		exit('SELECTクエリーが失敗しました。');
 	}
 
-	echo '<div class="link ad"><script type="text/javascript">alert("test")</script></div>';
+	echo '<div class="link ad"><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="142a8617.af4bd4b0.142a8618.1730d9f1";rakuten_items="ctsmatch";rakuten_genreId="0";rakuten_size="200x200";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";rakuten_ts="1603412175006";</script><script type="text/javascript" src="https://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script></div>';
 
 //	$linkNo = 5;
 	while($row = pg_fetch_assoc($result)){
