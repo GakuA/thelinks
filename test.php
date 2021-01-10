23facc9a8ee4a04a7e7f0cdd25dc20b115b62c95
アカザワ ガク <gaku@gaku-no-MacBook-Pro.local>
2021-01-10 17:13:00 +0900

    test.php

--- test.php
+++ test.php
@@ -1,2 +1,9 @@
 <?php
-	echo __DIR__;
+	$link = pg_connect('host=ec2-54-146-4-66.compute-1.amazonaws.com dbname=d92g50uce9vjgl user=zkwliroqdgiofi password=e63c65c7f1f58c3c4a2f30b27e52d34a736e0e0cf6f76f9abee7f663948bc692');
+	if (!$link) {
+		die('接続失敗です。');
+	}
+
+	pg_query("ALTER TABLE link ADD COLUMN img text;");
+
+	pg_close($link);
