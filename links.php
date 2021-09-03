fd7b2e1dceac082f68ab0f665c75485c505fa627
gaku akazawa <akazawagaku@gmail.com>
2021-09-03 10:40:39 +0900

    test

--- links.php
+++ links.php
@@ -9,7 +9,7 @@
 	if(!$result){
 		exit('SELECTクエリーが失敗しました。');
 	}
-//	$linkNo = 5;
+	$linkNo = 5;
 	while($row = pg_fetch_assoc($result)){
 		$url = $row["url"];
 		$title = $row["title"];
@@ -19,12 +19,12 @@
 		}
 		$html = '<div class="link"><a target="_blank" href="'.$url.'"><img src="'.$img.'"><span>'.$title.'</span></a></div>';
 		echo $html;
-/*
+
 		if ($linkNo == 6) {
 			echo '<div class="link ad"><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="142a8617.af4bd4b0.142a8618.1730d9f1";rakuten_items="ranking";rakuten_genreId="0";rakuten_size="200x200";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";rakuten_ts="1603296752086";</script><script type="text/javascript" src="https://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script></div>';
 		}
-*/
-		//$linkNo++;
+
+		$linkNo++;
 	}
 
 	pg_close($link);
