c23287bcf6444bd09e2fb52721bb7afeec25c2a4
gaku akazawa <akazawagaku@gmail.com>
2022-02-18 16:21:34 +0900

    ad+

--- js/load.js
+++ js/load.js
@@ -4,7 +4,22 @@ $(function() {
 		url: "links.php",
 		success: function(data) {
 			$("#links").html(data);
-			console.log($("#links .link").length);
+			//console.log($("#links .link").length);
+			// 複製するHTML要素を取得
+			var adPC = $(".adPC")[0];
+			var adSP = $(".adSP")[0];
+
+			// 複製
+			var clone_adPC = adPC.cloneNode(true);
+			var clone_adSP = adSP.cloneNode(true);
+
+			for (i=0; i<$("#links .link").not(".adPC, .adSP").length; i++) {
+				if (i % 10 == 9) {
+					// 複製したHTML要素をページに挿入
+					$("#links .link").not(".adPC, .adSP")[i].after(clone_adSP);
+					$("#links .link").not(".adPC, .adSP")[i].after(clone_adPC);
+				}
+			}
 		}
 	});
 });
