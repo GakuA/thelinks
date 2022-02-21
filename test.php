cdd360e6993e57362a7deeae29003e1e5118edc2
gaku akazawa <akazawagaku@gmail.com>
2022-02-21 10:03:15 +0900

    test

--- test.php
+++ test.php
@@ -3,7 +3,9 @@
 		$url = $_GET['url'];
 	}
 
-	if(preg_match("/<title.*?>(.*?)<\/title>/i", $url, $matches)){
+	$html = @file_get_contents($url);
+
+	if(preg_match("/<title.*?>(.*?)<\/title>/i", $html, $matches)){
 		$cord = mb_detect_encoding($matches[1], "UTF-8, ASCII, JIS, EUC-JP, SJIS, SHIFT_JIS");
 		if ($cord == "UTF-8") {
 			echo $matches[1];
