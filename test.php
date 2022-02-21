166efa8cfc2674c4be5326278accec5cad0f86f8
gaku akazawa <akazawagaku@gmail.com>
2022-02-21 09:58:55 +0900

    test

--- test.php
+++ test.php
@@ -1,2 +1,15 @@
 <?php
-	echo __DIR__;
+	if　(isset($_GET['url'])) {
+		$url = $_GET['url'];
+	}
+
+	if(preg_match("/<title.*?>(.*?)<\/title>/i", $url, $matches)){
+		$cord = mb_detect_encoding($matches[1], "UTF-8, ASCII, JIS, EUC-JP, SJIS, SHIFT_JIS");
+		if ($cord == "UTF-8") {
+			echo $matches[1];
+		} else {
+			echo mb_convert_encoding($matches[1], "UTF-8", $cord);
+		}
+	} else {
+		return false;
+	}
