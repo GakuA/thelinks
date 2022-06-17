a72a46d54d2a6c7b173ce782f3225de81ff9470b
gaku akazawa <akazawagaku@gmail.com>
2022-06-17 20:53:50 +0900

    メンテナンス

--- links.php
+++ links.php
@@ -1,7 +1,7 @@
 <?php
 	$link = pg_connect('host=ec2-54-146-4-66.compute-1.amazonaws.com dbname=d92g50uce9vjgl user=zkwliroqdgiofi password=e63c65c7f1f58c3c4a2f30b27e52d34a736e0e0cf6f76f9abee7f663948bc692');
 	if (!$link) {
-		die('接続失敗です。');
+		die('ただ今メンテナンス中です。');
 	}
 
 	$result = pg_query("SELECT * FROM link order by date desc");
