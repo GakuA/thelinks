2f7e96ad6fe89cb4d7a6852a5b16580d71e264d9
gaku akazawa <akazawagaku@gmail.com>
2022-06-24 10:01:48 +0900

    amazon

--- index.php
+++ index.php
@@ -140,5 +140,14 @@ akazawagaku@gmail.com
 			casper.run();
 		</script>
 		-->
+		<script type="text/javascript">
+		    amzn_assoc_ad_type = "link_enhancement_widget";
+		    amzn_assoc_tracking_id = "linkss-22";
+		    amzn_assoc_linkid = "c84fabded6a9f368fcd70b908bea9b82";
+		    amzn_assoc_placement = "";
+		    amzn_assoc_marketplace = "amazon";
+		    amzn_assoc_region = "JP";
+		</script>
+		<script src="//ws-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&MarketPlace=JP"></script>
 	</body>
 </html>
